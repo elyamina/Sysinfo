ad81ef43fc4feef3347fc24d016c62823d2e9b94
elyamina <gagarin095@gmail.com>
2016-12-21 21:24:45 +0400

    Add files via upload

--- sysinfo_Lyamina/install.sh
+++ sysinfo_Lyamina/install.sh
@@ -12,9 +12,9 @@ sudo cp -f $PWD/apache2_confs/000-default.conf /etc/apache2/sites-enabled/
 sudo cp -f $PWD/apache2_confs/000-default.conf /etc/apache2/sites-available/
 sudo cp -f $PWD/apache2_confs/ports.conf /etc/apache2/
 sudo cp -f $PWD/apache2_confs/index.html  /var/www/html/sysinfo/index.html
-sudo cp -R $PWD/apache2_confs/sysinfo/  /var/www/html/
+sudo cp -R $PWD/html/sysinfo/  /var/www/html/
 sudo cp -f $PWD/apache2_confs/serve-cgi-bin.conf  /etc/apache2/conf-available/
-sudo cp -R $PWD/apache2_confs/sysinfo_cgi-bin/ /var/www/html/ 
+sudo cp -R $PWD/html/sysinfo_cgi-bin/ /var/www/html/ 
 
 sudo ln -s /etc/apache2/mods-available/cgi.load /etc/apache2/mods-enabled/cgi.load
 
@@ -27,8 +27,8 @@ sudo chmod 777 /var/www/stat/
 sudo mkdir /var/www/stat/top_t
 sudo mkdir /usr/share/nginx/html/archive/
 
-sudo cp -r $PWD/html/sysinfo /var/www/html/
-sudo cp -r $PWD/html/sysinfo_cgi-bin/ /var/www/html/
+#sudo cp -r $PWD/html/sysinfo /var/www/html/
+#sudo cp -r $PWD/html/sysinfo_cgi-bin/ /var/www/html/
 #sudo cp $PWD/sysinfo_logger.sh /var/www/html/sysinfo_cgi-bin/
 #sudo cp  $PWD/netinfo_logger.sh /var/www/html/sysinfo_cgi-bin/
 #sudo cp  $PWD/loop_netstat.sh /var/www/html/sysinfo_cgi-bin/
